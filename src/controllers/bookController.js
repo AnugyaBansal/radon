a13bd87c587119a7ee8b47d66aa4c7be1804868e
AnugyaBansal <bansalanugya66@gmail.com>
2022-06-07 13:57:18 +0530

    books2

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -8,8 +8,35 @@ const createBook= async function (req, res) {
     res.send({msg: savedData})
 }
 
-const getBooksData= async function (req, res) {
+const getBooksList= async function (req, res) {
+    let allBooksList= await BookModel.find( ).select({bookName: 1, authorName: 1})
+    res.send({msg: allBooksList})
+}
 
+const getBooksInYear= async function (req, res) {
+    let publishyr = req.body.year
+    let allBooksList= await BookModel.find({year: publishyr})
+    res.send({msg: allBooksList})
+   
+}
+
+const getParticularBooks= async function (req, res) {
+    let condition=req.body
+    let allBooksList= await BookModel.find(req.body)
+    res.send({msg: allBooksList})
+} 
+
+const getXINRBooks= async function (req, res) {
+    let allBooksList= await BookModel.find({"prices.indianPrice":{$in:["INR100", "INR200", "INR500"]}})
+    res.send({msg: allBooksList})
+}
+
+const getRandomBooks= async function (req, res) {
+    let allBooksList= await BookModel.find({
+        $and:[{stockAvailable: true}, {totalPages:500} ]
+})
+    res.send({msg: allBooksList})
+}
     // let allBooks= await BookModel.find( ).count() // COUNT
 
     // let allBooks= await BookModel.find( { authorName : "Chetan Bhagat" , isPublished: true  } ) // AND
@@ -65,21 +92,24 @@ const getBooksData= async function (req, res) {
     
     // ASYNC AWAIT
     
-    let a= 2+4
-    a= a + 10
-    console.log(a)
-    let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
+    // let a= 2+4
+    // a= a + 10
+    // console.log(a)
+    //let allBooks= await BookModel.find( )  //normally this is an asynchronous call..but await makes it synchronous
 
 
     // WHEN AWAIT IS USED: - database + axios
     //  AWAIT can not be used inside forEach , map and many of the array functions..BE CAREFUL
-    console.log(allBooks)
-    let b = 14
-    b= b+ 10
-    console.log(b)
-    res.send({msg: allBooks})
-}
+    // console.log(allBooks)
+    // let b = 14
+    // b= b+ 10
+    // console.log(b)
+  
 
 
 module.exports.createBook= createBook
-module.exports.getBooksData= getBooksData
+module.exports.getBooksList= getBooksList
+module.exports.getBooksInYear= getBooksInYear
+module.exports.getXINRBooks= getXINRBooks
+module.exports.getRandomBooks= getRandomBooks
+module.exports.getParticularBooks= getParticularBooks
--- src/routes/route.js
+++ src/routes/route.js
@@ -8,12 +8,15 @@ router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
 })
 
-router.post("/createUser", UserController.createUser  )
+// router.post("/createUser", UserController.createUser  )
 
-router.get("/getUsersData", UserController.getUsersData)
+// router.get("/getUsersData", UserController.getUsersData)
 
 router.post("/createBook", BookController.createBook  )
-
-router.get("/getBooksData", BookController.getBooksData)
+router.get("/getBooksList", BookController.getBooksList)
+router.post("/getBooksInYear", BookController.getBooksInYear)
+router.get("/getXINRBooks", BookController.getXINRBooks)
+router.get("/getRandomBooks", BookController.getRandomBooks)
+router.get("/getParticularBooks", BookController.getParticularBooks)
 
 module.exports = router;
