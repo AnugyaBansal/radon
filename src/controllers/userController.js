00f6e67d97d1de17a254bcbc47e95aad3aae0a3c
AnugyaBansal <bansalanugya66@gmail.com>
2022-06-10 22:49:59 +0530

    W4D5

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,25 +11,6 @@ const basicCode= async function(req, res) {
     console.log( "hey man, congrats you have reached the Handler")
     res.send({ msg: "This is coming from controller (handler)"})
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const createUser= async function (req, res) {
     let data= req.body
     let savedData= await UserModel.create(data)
@@ -41,6 +22,22 @@ const getUsersData= async function (req, res) {
     res.send({msg: allUsers})
 }
 
+
+const mid= function(req, res, next){
+    let currentDate= new Date();
+    let dateTime= currentDate.getDate() + "-"
+                  +(currentDate.getMonth()+1) + "-"
+                  +currentDate.getFullYear() + " "
+                  +currentDate.getHours() + ":"
+                  +currentDate.getMinutes() + ":"
+                  +currentDate.getSeconds(); 
+    let ip= req.ip
+    let url= req.url
+    console.log(`${dateTime}, ${ip}, ${url}`)
+    res.send(`${dateTime}, ${ip}, ${url}`)
+    next()
+}
+module.exports.mid=mid
 module.exports.createUser= createUser
 module.exports.getUsersData= getUsersData
 module.exports.basicCode= basicCode
